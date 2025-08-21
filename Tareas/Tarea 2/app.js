48f7dc489e7687a316bb1dd6d63f3dcd0ee6fde4
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2025-08-20 22:09:09 -0400

    Tarea 2

---
+++ Tareas/Tarea 2/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const app = express();
+
+// Conexión a MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/agendaDB")
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.error(err));
+
+// Configuraciones
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// Importar modelo
+const Agenda = require("./models/Agenda");
+
+// Rutas
+
+// 📌 LISTAR todos
+app.get("/", async (req, res) => {
+  const contactos = await Agenda.find();
+  res.render("index", { contactos });
+});
+
+// 📌 FORMULARIO CREAR
+app.get("/new", (req, res) => {
+  res.render("create");
+});
+
+// 📌 CREAR nuevo
+app.post("/", async (req, res) => {
+  await Agenda.create(req.body);
+  res.redirect("/");
+});
+
+// 📌 FORMULARIO EDITAR
+app.get("/edit/:id", async (req, res) => {
+  const contacto = await Agenda.findById(req.params.id);
+  res.render("edit", { contacto });
+});
+
+// 📌 ACTUALIZAR
+app.put("/:id", async (req, res) => {
+  await Agenda.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/");
+});
+
+// 📌 ELIMINAR
+app.delete("/:id", async (req, res) => {
+  await Agenda.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+// Servidor
+app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
